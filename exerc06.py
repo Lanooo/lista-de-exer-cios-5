0cf70199fd6ed33a5e7a95af241b4c6dde64cd89
Helano Rangel do Prado <89919132+Lanooo@users.noreply.github.com>
2021-09-18 11:44:33 -0300

    Add files via upload

---
+++ exerc06.py
@@ -0,0 +1,13 @@
+n1 = int(input("Valor do primeiro produto: "))
+n2 = int(input("Valor do segundo produto: "))
+
+d1 = (n1*8)/100
+d2 = (n2*11)/100
+
+v1 = n1 - d1
+v2 = n2 - d2
+vf = v1 + v2
+
+print (v1)
+print (v2)
+print ("O valor final a ser pago é: ", vf)
