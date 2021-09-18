b989295fff3afac5a9c8025f4f8552ccebb89cb5
Helano Rangel do Prado <89919132+Lanooo@users.noreply.github.com>
2021-09-18 13:16:09 -0300

    Update exerc07.py

--- exerc07.py
+++ exerc07.py
@@ -1,3 +1,8 @@
+#Escreva um algoritmo que receba dois números e exiba para o usuário todos os valores intermediários a eles, veja exemplo:
+#Primeiro número: 5
+#Segundo número: 15
+#Resultado: 6 7 8 9 10 11 12 13 14#
+
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 
