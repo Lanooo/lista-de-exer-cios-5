d06fad43ecd008ffc5e39a908ce818e7146ed224
Helano Rangel do Prado <89919132+Lanooo@users.noreply.github.com>
2021-09-18 13:02:21 -0300

    Update exerc05.py

--- exerc05.py
+++ exerc05.py
@@ -1,3 +1,5 @@
+#Escreva um algoritmo para calcular o fatorial de um número. Se o usuário digitou, por exemplo, o valor 5, o resultado a ser exibido pelo algoritmo é: 5! é igual a 120
+
 n = int(input("Fatorial de: ") )
 
 r = 1
@@ -7,4 +9,4 @@ while count <= n:
     r *= count
     count += 1
 
-print(r)
+print(f"{n}! é igual a {r}")
