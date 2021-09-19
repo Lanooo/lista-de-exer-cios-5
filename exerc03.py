de4f933ec7b929a4707f1a96b5db60191952ba82
Helano Rangel do Prado <89919132+Lanooo@users.noreply.github.com>
2021-09-19 16:44:01 -0300

    Update exerc03.py

--- exerc03.py
+++ exerc03.py
@@ -6,6 +6,6 @@ while cont <= 10:
     ddn = int(input("Digite sua data de nascimento: "))
     cont = cont + 1
     idade = 2021 - ddn
-    if idade > 18:
+    if idade >= 18:
         acumulador = acumulador + 1
 print (acumulador,"pessoas são maiores de idade.")
